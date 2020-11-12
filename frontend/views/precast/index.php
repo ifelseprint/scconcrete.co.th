b0a6de1330931228c29e821b5f87e11149cee0dc
K <titirat.pa@allianz.com>
2020-11-13 01:46:10 +0700

    Add : page precast

---
+++ frontend/views/precast/index.php
@@ -0,0 +1,123 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use frontend\assets\PrecastAsset;
+PrecastAsset::register($this);
+?>
+<main class="main">
+	<div class="precast-post">
+        
+        <div class="precast-block">
+        	<div class="container precast-block-bg">
+        		<div class="precast-block-bg-text"><img src="<?=Url::base(true);?>/img/precast_text_v.jpg" width="50"></div>
+        		<div class="row">
+        			<div class="col-md-12">
+        				<img src="<?=Url::base(true);?>/img/product_title.jpg" width="300">
+        			</div>
+        		</div>
+        		<div class="row">
+        			<div class="col-md-8 col-md-offset-2">
+        				<div class="precast-block-title">
+        					<h3>SC PRECAST</h3>
+        					<h4>คอนกรีตอัดแรงสำเร็จรูป</h4>
+        				</div>
+        				<div class="precast-block-content">
+        					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mauris sit amet quam cursus egestas. Duis porta augue sapien, id ultrices lectus rhoncus et. Nulla pellentesque libero vitae magna pharetra vulputate. Nullam urna leo, pharetra ut lacinia quis, consectetur quis est. In vehicula, dui id semper commodo, mi orci rhoncus velit, nec pretium mauris orci vel velit. Nunc gravida euismod scelerisque. Suspendisse tellus sem, eleifend id finibus sit amet, elementum ac quam. Praesent gravida metus est, vitae feugiat erat dignissim molestie. Vivamus sit amet arcu nec odio maximus placerat in ac metus. Donec vulputate purus non pharetra ultricies. Integer vestibulum tellus condimentum nunc iaculis, id dignissim ligula convallis. Morbi rhoncus semper justo eu aliquet. Donec in nunc lectus. Sed mollis massa at enim rutrum, vel dapibus tortor interdum.
+        				</div>
+        				<div class="precast-block-banner">
+        					<img src="<?=Url::base(true);?>/img/precast_banner.jpg" width="100%">
+        				</div>
+        				<div class="precast-block-banner-footer">
+        					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mauris sit amet quam cursus egestas. Duis porta augue sapien, id ultrices lectus rhoncus et. Nulla pellentesque libero vitae magna pharetra vulputate. Nullam urna leo, pharetra ut lacinia quis, consectetur quis est. In vehicula, dui id semper commodo, mi orci rhoncus velit, nec pretium mauris orci vel velit. Nunc gravida euismod scelerisque. Suspendisse tellus sem,
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </div>
+       	<div class="precast-block-list">
+       		<div class="container">
+	        	<?php
+				//Columns must be a factor of 12 (1,2,3,4,6,12)
+				// $numOfCols = 3;
+				// $rowCount = 0;
+				// $bootstrapColWidth = 12 / $numOfCols;
+				?>
+				<div class="row">
+				<?php
+				// foreach ($precast as $value){
+				// 	$precast_name = 'precast_name_'.Yii::$app->language;
+				// 	$precast_content = 'precast_content_'.Yii::$app->language;
+				?>  
+			        <div class="col-md-4">
+	    				<div class="precast-box">
+		    				<div class="precast-image">
+		    					<img src="<?=Url::base(true);?>/img/precast1.jpg" width="100%">
+							</div>
+		    				<div class="precast-title">
+		    					ท่อคอนกรีตสำเร็จรูป (ท่อกลม)
+		    				</div>
+		    			</div>
+			        </div>
+			        <div class="col-md-4">
+	    				<div class="precast-box">
+		    				<div class="precast-image">
+		    					<img src="<?=Url::base(true);?>/img/precast2.jpg" width="100%">
+							</div>
+		    				<div class="precast-title">
+		    					ท่อคอนกรีตสำเร็จรูป (ท่อกลม)
+		    				</div>
+		    			</div>
+			        </div>
+			        <div class="col-md-4">
+	    				<div class="precast-box">
+		    				<div class="precast-image">
+		    					<img src="<?=Url::base(true);?>/img/precast3.jpg" width="100%">
+							</div>
+		    				<div class="precast-title">
+		    					ท่อคอนกรีตสำเร็จรูป (ท่อกลม)
+		    				</div>
+		    			</div>
+			        </div>
+
+			        <div class="col-md-4">
+	    				<div class="precast-box">
+		    				<div class="precast-image">
+		    					<img src="<?=Url::base(true);?>/img/precast4.jpg" width="100%">
+							</div>
+		    				<div class="precast-title">
+		    					ท่อคอนกรีตสำเร็จรูป (ท่อกลม)
+		    				</div>
+		    			</div>
+			        </div>
+			        <div class="col-md-4">
+	    				<div class="precast-box">
+		    				<div class="precast-image">
+		    					<img src="<?=Url::base(true);?>/img/precast5.jpg" width="100%">
+							</div>
+		    				<div class="precast-title">
+		    					ท่อคอนกรีตสำเร็จรูป (ท่อกลม)
+		    				</div>
+		    			</div>
+			        </div>
+			        <div class="col-md-4">
+	    				<div class="precast-box">
+		    				<div class="precast-image">
+		    					<img src="<?=Url::base(true);?>/img/precast6.jpg" width="100%">
+							</div>
+		    				<div class="precast-title">
+		    					ท่อคอนกรีตสำเร็จรูป (ท่อกลม)
+		    				</div>
+		    			</div>
+			        </div>
+				<?php
+				//     $rowCount++;
+				//     if($rowCount % $numOfCols == 0) echo '</div><div class="row">';
+				// }
+				?>
+				</div>
+
+	        	
+	        </div>
+        </div>
+    </div>
+</main>
