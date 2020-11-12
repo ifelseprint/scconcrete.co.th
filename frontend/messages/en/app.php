05de0a3a1c1267c3395eebaa02200e66393d89ab
K <titirat.pa@allianz.com>
2020-11-13 01:42:52 +0700

    Fix : lang menu and hyber link

--- frontend/messages/en/app.php
+++ frontend/messages/en/app.php
@@ -2,9 +2,12 @@
 return [
 	'menu_home' => 'home',
 	'menu_about' => 'about us',
-    'menu_product' => 'product',
-    'menu_article' => 'article',
-    'menu_service' => 'we care',
+    'menu_precast' => 'sc precast',
+    'menu_concrete' => 'sc concrete',
+    'menu_news' => 'news',
+    'menu_portfolio' => 'portfolio',
+    'menu_property' => 'property',
+    'menu_project' => 'project',
     'menu_join_us' => 'join us',
     'menu_contact_us' => 'contact us',
 ];
--- frontend/messages/th/app.php
+++ frontend/messages/th/app.php
@@ -1,10 +1,13 @@
 <?php
 return [
 	'menu_home' => 'หน้าแรก',
-	'menu_about' => 'เกี่ยวกับเรา',
-    'menu_product' => 'สินค้า',
-    'menu_article' => 'บทความ',
-    'menu_service' => 'we care',
+	'menu_about' => 'รู้จักเรา',
+    'menu_precast' => 'คอนกรีตอัดแรงสำเร็จรูป',
+    'menu_concrete' => 'คอนกรีตผสมเสร็จ',
+    'menu_news' => 'กิจกรรมและข่าวสาร',
+    'menu_portfolio' => 'ผลงานที่ผ่านมา',
+    'menu_property' => 'อสังหาริมทรัพท์',
+    'menu_project' => 'โครงการ',
     'menu_join_us' => 'ร่วมงานกับเรา',
     'menu_contact_us' => 'ติดต่อเรา',
 ];
