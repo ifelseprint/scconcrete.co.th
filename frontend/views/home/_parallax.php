89291eb8cc99ad112fd7b4db010bcc9ff37939a2
K <titirat.pa@allianz.com>
2020-11-13 01:46:32 +0700

    Fix : Page home parallax

--- frontend/views/home/_parallax.php
+++ frontend/views/home/_parallax.php
@@ -4,6 +4,4 @@ use yii\helpers\Url;
 ?>
 <hr style="margin: 7px 0px;border-top: 5px solid #e10000;">
 <div style="background-image: url(img/palalax-bg.jpg);min-height: 400px; background-attachment: fixed;background-position: center;background-repeat: no-repeat;background-size: cover;">
-    xxx
 </div>
-<hr style="margin: 7px 0px;border-top: 5px solid #e10000;">
